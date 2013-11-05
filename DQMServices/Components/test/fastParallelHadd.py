56b0b12ee279e96856f43f664a65e205fd73388c
Marco Rovere <marco.rovere@cern.ch>
2013-11-06 00:06:40 +0100

    Simplified the logic of the parallel algorithm

--- DQMServices/Components/test/fastParallelHadd.py
+++ DQMServices/Components/test/fastParallelHadd.py
@@ -40,7 +40,8 @@ class SampleManager:
         print self.files_
         self.low_index_ = 0
         self.up_index_ = len(self.files_)
-        self.group_by_ = opt.group_by
+        self.abs_index_ = len(self.files_)
+        self.group_by_ = max(2, opt.group_by) # no point in adding 1 file.
         self.num_threads_ = opt.num_threads
         self.output_file_ = opt.output_file
         self.debug_ = opt.debug
@@ -50,31 +51,33 @@ class SampleManager:
         self.lock_.acquire()
         self.files_.append(input_file)
         self.nukefiles_.append(input_file)
+        self.up_index_ = self.up_index_ + 1
         if self.debug_ > 0:
-            print "Inserting %s" % input_file
+            print "Inserting %s\n" % input_file
         self.lock_.release()
 
     def processFiles(self):
-        while self.up_index_ - self.low_index_ > self.group_by_ - 1:
-            while activeCount() > self.num_threads_:
-                pass
-            if self.debug_ > 0:
-                print "Step: %d" % self.step_
-            f = []
-            for j in range(0, self.group_by_):
-                while len(self.files_) <= self.low_index_:
+        while True:
+            while self.up_index_ - self.low_index_ >= self.group_by_:
+                while activeCount() > self.num_threads_:
                     pass
-                f.append(self.files_[self.low_index_])
-                self.low_index_ += 1
-                if self.debug_ > 1:
-                    print "low_index: %d, up_index:%d" % (self.low_index_,
-                                                          self.up_index_)
-            if len(f) > 0:
-                t = SingleMerge(self, "%d.pb" % (self.up_index_),
-                                self.debug_, *f)
-                t.start()
-                self.up_index_ += 1
-                self.step_ += 1
+                if self.debug_ > 0:
+                    print "Step: %d" % self.step_
+                f = []
+                for j in range(0, self.group_by_):
+                    if self.debug_ > 1:
+                        print "low_index: %d, up_index:%d" % (self.low_index_,
+                                                              self.up_index_)
+                    f.append(self.files_[self.low_index_])
+                    self.low_index_ += 1
+                if len(f) > 0:
+                    t = SingleMerge(self, "%d.pb" % (self.abs_index_),
+                                    self.debug_, *f)
+                    t.start()
+                    self.abs_index_ += 1
+                    self.step_ += 1
+            if activeCount() == 1:
+                break
 
         # Consume whatever is left, in a single step, since we exited the
         # previous while
@@ -83,17 +86,16 @@ class SampleManager:
             while (self.low_index_ + 1) <= self.up_index_:
                 if self.debug_ > 2:
                     print "Currently at ", self.low_index_, self.up_index_, len(self.files_)
-                while len(self.files_) <= self.low_index_:
-                    pass
                 f.append(self.files_[self.low_index_])
                 self.low_index_ += 1
             if len(f) > 0 :
                 if self.debug_ > 0:
                     print "Final Step: %d" % self.step_
-                t = SingleMerge(self, "%d.pb" % (self.up_index_),
+                t = SingleMerge(self, "%d.pb" % (self.abs_index_),
                                 self.debug_, *f)
+                self.abs_index_ += 1
                 t.start()
-                self.up_index_ += 1
+
         while activeCount() > 1:
             pass
         self.finalise()
