db051b120e6e5739924675d314ec42283800c4dc
Marco Rovere <marco.rovere@cern.ch>
2016-07-09 14:43:59 +0200

    Very much WIP

--- RecoPixelVertexing/PixelTriplets/plugins/CellularAutomaton.h
+++ RecoPixelVertexing/PixelTriplets/plugins/CellularAutomaton.h
@@ -1,36 +1,37 @@
 #ifndef RECOPIXELVERTEXING_PIXELTRIPLETS_PLUGINS_CELLULARAUTOMATON_H_
 #define RECOPIXELVERTEXING_PIXELTRIPLETS_PLUGINS_CELLULARAUTOMATON_H_
 #include <array>
+#include <unordered_map>
 #include "CACell.h"
 #include "TrackingTools/TransientTrackingRecHit/interface/SeedingLayerSetsHits.h"
 #include "RecoTracker/TkTrackingRegions/interface/TrackingRegion.h"
 
 template<unsigned int theNumberOfLayers>
 class CellularAutomaton {
-public:
+ public:
+  CellularAutomaton() {}
 
-    CellularAutomaton() {
+  void create_and_connect_cells(const std::map<std::pair<int, int>, HitDoublets> &,
+                                const std::unordered_map<int, int>&,
+                                const TrackingRegion&,
+                                const float, const float);
+  void evolve();
+  void find_ntuplets(std::vector<CACell::CAntuplet>&, const unsigned int);
 
-    }
-
-    void create_and_connect_cells(std::array<const HitDoublets*, theNumberOfLayers-1> const&, const SeedingLayerSetsHits::SeedingLayerSet&, const TrackingRegion&, const float, const float);
-    void evolve();
-    void find_ntuplets(std::vector<CACell::CAntuplet>&, const unsigned int);
-
-
-
-private:
-
-
-
-    //for each hit in each layer, store the pointers of the Cells of which it is outerHit
-    std::array<std::vector<std::vector<CACell*> >, theNumberOfLayers> isOuterHitOfCell;
-    std::array<std::vector<CACell>, theNumberOfLayers> theFoundCellsPerLayer;
-
-    std::vector<CACell*> theRootCells;
-    std::vector<std::vector<CACell*> > theNtuplets;
-
+ private:
+  //for each hit in each layer, store the pointers of the Cells of which it is outerHit
+  std::array<std::vector<std::vector<CACell*> >, theNumberOfLayers> isOuterHitOfCell;
+  std::array<std::vector<CACell>, theNumberOfLayers> theFoundCellsPerLayer;
+  std::vector<CACell*> theRootCells;
+  std::vector<std::vector<CACell*> > theNtuplets;
+  std::array<std::string, theNumberOfLayers> layers_;
 };
 
 
 #endif 
+
+
+
+
+
+
