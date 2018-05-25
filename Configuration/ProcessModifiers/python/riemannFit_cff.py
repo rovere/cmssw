3de53b17a12b19bc8fd2a0a7913e042d019a2f8e
Marco Rovere <marco.rovere@cern.ch>
2018-05-25 14:25:37 +0200

    Add riemannFitGPU subera and workflows

--- Configuration/ProcessModifiers/python/riemannFit_cff.py
+++ Configuration/ProcessModifiers/python/riemannFit_cff.py
@@ -3,3 +3,4 @@ import FWCore.ParameterSet.Config as cms
 # This modifier is for replacing the default pixel track "fitting" with Riemann fit
 
 riemannFit =  cms.Modifier()
+riemannFitGPU =  cms.Modifier()
