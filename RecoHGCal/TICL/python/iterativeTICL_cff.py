6c501e90b0554ce3c16f021f2c80cddea23acf0e
Marco Rovere <marco.rovere@cern.ch>
2019-11-12 11:35:24 +0100

    Simplify TICL reconstruction for the tutorial

--- RecoHGCal/TICL/python/iterativeTICL_cff.py
+++ RecoHGCal/TICL/python/iterativeTICL_cff.py
@@ -7,21 +7,12 @@ from RecoHGCal.TICL.HADStep_cff import *
 from RecoHGCal.TICL.ticlLayerTileProducer_cfi import ticlLayerTileProducer
 from RecoHGCal.TICL.ticlCandidateFromTrackstersProducer_cfi import ticlCandidateFromTrackstersProducer as _ticlCandidateFromTrackstersProducer
 from RecoHGCal.TICL.pfTICLProducer_cfi import pfTICLProducer
-from RecoHGCal.TICL.trackstersMergeProducer_cfi import trackstersMergeProducer as _trackstersMergeProducer
 from RecoHGCal.TICL.multiClustersFromTrackstersProducer_cfi import multiClustersFromTrackstersProducer as _multiClustersFromTrackstersProducer
 
 
 ticlLayerTileTask = cms.Task(ticlLayerTileProducer)
 
-trackstersMerge = _trackstersMergeProducer.clone()
-multiClustersFromTrackstersMerge = _multiClustersFromTrackstersProducer.clone(
-    Tracksters = "trackstersMerge"
-)
-ticlTracksterMergeTask = cms.Task(trackstersMerge, multiClustersFromTrackstersMerge)
-
-ticlCandidateFromTrackstersProducer = _ticlCandidateFromTrackstersProducer.clone(
-      tracksterCollections = ["trackstersMerge"]
-    )
+ticlCandidateFromTrackstersProducer = _ticlCandidateFromTrackstersProducer.clone()
 ticlPFTask = cms.Task(ticlCandidateFromTrackstersProducer, pfTICLProducer)
 
 iterTICLTask = cms.Task(ticlLayerTileTask
@@ -29,7 +20,6 @@ iterTICLTask = cms.Task(ticlLayerTileTask
     ,EMStepTask
     ,TrkStepTask
     ,HADStepTask
-    ,ticlTracksterMergeTask
     ,ticlPFTask
     )
 
