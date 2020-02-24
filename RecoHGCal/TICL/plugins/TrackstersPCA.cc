000e78294c41fb9358c2b374a59e9ca0c832bcb8
Leonardo Cristella <leonardo.cristella@cern.ch>
2020-02-24 19:09:10 +0100

    Add flag for energy-weigthed (default) PCA

--- RecoHGCal/TICL/plugins/TrackstersPCA.cc
+++ RecoHGCal/TICL/plugins/TrackstersPCA.cc
@@ -6,7 +6,7 @@
 
 void ticl::assignPCAtoTracksters(std::vector<Trackster> & tracksters,
     const std::vector<reco::CaloCluster> &layerClusters, double z_limit_em, bool energyWeight) {
-  TPrincipal pca(3, "");
+  TPrincipal pca(3, "D"); // D option is necessary to use the GetRow method
   LogDebug("TrackstersPCA") << "-------" << std::endl;
   for (auto &trackster : tracksters) {
     pca.Clear();
@@ -19,13 +19,14 @@ void ticl::assignPCAtoTracksters(std::vector<Trackster> & tracksters,
       trackster.raw_energy += layerClusters[trackster.vertices[i]].energy() * fraction;
       if (std::abs(layerClusters[trackster.vertices[i]].z()) <= z_limit_em)
         trackster.raw_em_energy += layerClusters[trackster.vertices[i]].energy() * fraction;
-      double point[3] = {
-        layerClusters[trackster.vertices[i]].x(),
-        layerClusters[trackster.vertices[i]].y(),
-        layerClusters[trackster.vertices[i]].z()
-      };
-      if (!energyWeight)
+      if (!energyWeight) {
+        double point[3] = {
+          layerClusters[trackster.vertices[i]].x(),
+          layerClusters[trackster.vertices[i]].y(),
+          layerClusters[trackster.vertices[i]].z()
+        };
         pca.AddRow(&point[0]);
+      }
     }
 
     float weights_sum = 0.f;
@@ -49,8 +50,18 @@ void ticl::assignPCAtoTracksters(std::vector<Trackster> & tracksters,
     trackster.barycenter = ticl::Trackster::Vector((*(pca.GetMeanValues()))[0],
         (*(pca.GetMeanValues()))[1],
         (*(pca.GetMeanValues()))[2]);
+    double sigmasPCA[3] = {0};
+    for (size_t i = 0; i < trackster.vertices.size(); ++i) {
+      double p[3];
+      pca.X2P(pca.GetRow(i), p);
+      sigmasPCA[0] += (p[0] - trackster.barycenter.x()) * (p[0] - trackster.barycenter.x());
+      sigmasPCA[1] += (p[1] - trackster.barycenter.y()) * (p[1] - trackster.barycenter.y());
+      sigmasPCA[2] += (p[2] - trackster.barycenter.z()) * (p[2] - trackster.barycenter.z());
+    }
     for (size_t i=0; i<3; ++i) {
+      sigmasPCA[i] = std::sqrt(sigmasPCA[i]/trackster.vertices.size());
       trackster.sigmas[i] = (float)(*(pca.GetSigmas()))[i];
+      trackster.sigmasPCA[i] = sigmasPCA[i];
       trackster.eigenvalues[i] = (float)(*(pca.GetEigenValues()))[i];
       trackster.eigenvectors[i] = ticl::Trackster::Vector((*(pca.GetEigenVectors()))[0][i],
         (*(pca.GetEigenVectors()))[1][i],
@@ -68,6 +79,7 @@ void ticl::assignPCAtoTracksters(std::vector<Trackster> & tracksters,
     const auto & eigenvectors = *(pca.GetEigenVectors());
     const auto & eigenvalues = *(pca.GetEigenValues());
     const auto & sigmas = *(pca.GetSigmas());
+
     LogDebug("TrackstersPCA") << "Trackster characteristics: " << std::endl;
     LogDebug("TrackstersPCA") << "Size: " << trackster.vertices.size() << std::endl;
     LogDebug("TrackstersPCA") << "Mean: " << mean[0] << ", " << mean[1] << ", " << mean[2] << std::endl;
@@ -77,5 +89,18 @@ void ticl::assignPCAtoTracksters(std::vector<Trackster> & tracksters,
     LogDebug("TrackstersPCA") << "EigeVectors 2: " << eigenvectors(0, 1) << ", " << eigenvectors(1, 1) << ", " << eigenvectors(2, 1) <<std::endl;
     LogDebug("TrackstersPCA") << "EigeVectors 3: " << eigenvectors(0, 2) << ", " << eigenvectors(1, 2) << ", " << eigenvectors(2, 2) <<std::endl;
     LogDebug("TrackstersPCA") << "Sigmas: " << sigmas[0] << ", " << sigmas[1] << ", " << sigmas[2] << std::endl;
+    LogDebug("TrackstersPCA") << "SigmasPCA: " << sigmasPCA[0] << ", " << sigmasPCA[1] << ", " << sigmasPCA[2] << std::endl;
+
+    std::cout << "Trackster characteristics: " << std::endl;
+    std::cout << "Size: " << trackster.vertices.size() << std::endl;
+    std::cout << "Energy: " << trackster.raw_energy << std::endl;
+    std::cout << "Mean: " << mean[0] << ", " << mean[1] << ", " << mean[2] << std::endl;
+    std::cout << "Weights sum:" << weights_sum << std::endl;
+    std::cout << "EigenValues: " << eigenvalues[0] << ", " << eigenvalues[1] << ", " << eigenvalues[2]  << std::endl;
+    std::cout << "EigeVectors 1: " << eigenvectors(0, 0) << ", " << eigenvectors(1, 0) << ", " << eigenvectors(2, 0) <<std::endl;
+    std::cout << "EigeVectors 2: " << eigenvectors(0, 1) << ", " << eigenvectors(1, 1) << ", " << eigenvectors(2, 1) <<std::endl;
+    std::cout << "EigeVectors 3: " << eigenvectors(0, 2) << ", " << eigenvectors(1, 2) << ", " << eigenvectors(2, 2) <<std::endl;
+    std::cout << "Sigmas: " << sigmas[0] << ", " << sigmas[1] << ", " << sigmas[2] << std::endl;
+    std::cout << "SigmasPCA: " << sigmasPCA[0] << ", " << sigmasPCA[1] << ", " << sigmasPCA[2] << std::endl;
   }
 }
