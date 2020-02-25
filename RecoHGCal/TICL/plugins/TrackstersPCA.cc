65413021bf8a5352967478988882e24e83ad46d9
Leonardo Cristella <leonardo.cristella@cern.ch>
2020-02-25 09:10:31 +0100

    Do not store input data

--- RecoHGCal/TICL/plugins/TrackstersPCA.cc
+++ RecoHGCal/TICL/plugins/TrackstersPCA.cc
@@ -6,7 +6,7 @@
 
 void ticl::assignPCAtoTracksters(std::vector<Trackster> & tracksters,
     const std::vector<reco::CaloCluster> &layerClusters, double z_limit_em, bool energyWeight) {
-  TPrincipal pca(3, "D"); // D option is necessary to use the GetRow method
+  TPrincipal pca(3, "N");
   LogDebug("TrackstersPCA") << "-------" << std::endl;
   for (auto &trackster : tracksters) {
     pca.Clear();
@@ -14,17 +14,19 @@ void ticl::assignPCAtoTracksters(std::vector<Trackster> & tracksters,
     trackster.raw_em_energy = 0.;
     trackster.raw_pt = 0.;
     trackster.raw_em_pt = 0.;
+    std::vector<double*> points(trackster.vertices.size());
     for (size_t i = 0; i < trackster.vertices.size(); ++i) {
       auto fraction = 1.f / trackster.vertex_multiplicity[i];
       trackster.raw_energy += layerClusters[trackster.vertices[i]].energy() * fraction;
       if (std::abs(layerClusters[trackster.vertices[i]].z()) <= z_limit_em)
         trackster.raw_em_energy += layerClusters[trackster.vertices[i]].energy() * fraction;
+      double point[3] = {
+        layerClusters[trackster.vertices[i]].x(),
+        layerClusters[trackster.vertices[i]].y(),
+        layerClusters[trackster.vertices[i]].z()
+      };
+      points[i] = point;
       if (!energyWeight) {
-        double point[3] = {
-          layerClusters[trackster.vertices[i]].x(),
-          layerClusters[trackster.vertices[i]].y(),
-          layerClusters[trackster.vertices[i]].z()
-        };
         pca.AddRow(&point[0]);
       }
     }
@@ -53,7 +55,7 @@ void ticl::assignPCAtoTracksters(std::vector<Trackster> & tracksters,
     double sigmasPCA[3] = {0};
     for (size_t i = 0; i < trackster.vertices.size(); ++i) {
       double p[3];
-      pca.X2P(pca.GetRow(i), p);
+      pca.X2P(points[i], p);
       sigmasPCA[0] += (p[0] - trackster.barycenter.x()) * (p[0] - trackster.barycenter.x());
       sigmasPCA[1] += (p[1] - trackster.barycenter.y()) * (p[1] - trackster.barycenter.y());
       sigmasPCA[2] += (p[2] - trackster.barycenter.z()) * (p[2] - trackster.barycenter.z());
@@ -91,7 +93,7 @@ void ticl::assignPCAtoTracksters(std::vector<Trackster> & tracksters,
     LogDebug("TrackstersPCA") << "Sigmas: " << sigmas[0] << ", " << sigmas[1] << ", " << sigmas[2] << std::endl;
     LogDebug("TrackstersPCA") << "SigmasPCA: " << sigmasPCA[0] << ", " << sigmasPCA[1] << ", " << sigmasPCA[2] << std::endl;
 
-    std::cout << "Trackster characteristics: " << std::endl;
+    std::cout << "\nTrackster characteristics: " << std::endl;
     std::cout << "Size: " << trackster.vertices.size() << std::endl;
     std::cout << "Energy: " << trackster.raw_energy << std::endl;
     std::cout << "Mean: " << mean[0] << ", " << mean[1] << ", " << mean[2] << std::endl;
