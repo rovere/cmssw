2713db3ef362a44fe1abed0e148a6a8f1bd04b10
Felice <felice.pantaleo@cern.ch>
2020-09-30 12:32:35 +0200

    Created a ClusterFilter by Algo, Size and Layer Range

--- RecoHGCal/TICL/plugins/ClusterFilterByAlgoAndSizeAndLayerRange.h
+++ RecoHGCal/TICL/plugins/ClusterFilterByAlgoAndSizeAndLayerRange.h
@@ -1,8 +1,8 @@
 // Authors: Marco Rovere - marco.rovere@cern.ch, Felice Pantaleo - felice.pantaleo@cern.ch
 // Date: 09/2020
 
-#ifndef RecoHGCal_TICL_ClusterFilterByAlgoAndLayerRange_H__
-#define RecoHGCal_TICL_ClusterFilterByAlgoAndLayerRange_H__
+#ifndef RecoHGCal_TICL_ClusterFilterByAlgoAndSizeAndLayerRange_H__
+#define RecoHGCal_TICL_ClusterFilterByAlgoAndSizeAndLayerRange_H__
 
 #include "DataFormats/CaloRecHit/interface/CaloCluster.h"
 #include "ClusterFilterBase.h"
@@ -12,14 +12,16 @@
 
 // Filter clusters that belong to a specific algorithm
 namespace ticl {
-  class ClusterFilterByAlgoAndLayerRange final : public ClusterFilterBase {
+  class ClusterFilterByAlgoAndSizeAndLayerRange final : public ClusterFilterBase {
   public:
-    ClusterFilterByAlgoAndLayerRange(const edm::ParameterSet& ps)
+    ClusterFilterByAlgoAndSizeAndLayerRange(const edm::ParameterSet& ps)
         : ClusterFilterBase(ps),
           algo_number_(ps.getParameter<int>("algo_number")),
+          min_cluster_size_(ps.getParameter<int>("min_cluster_size")),
+          max_cluster_size_(ps.getParameter<int>("max_cluster_size")),
           min_layerId_(ps.getParameter<int>("min_layerId")),
           max_layerId_(ps.getParameter<int>("max_layerId")) {}
-    ~ClusterFilterByAlgoAndLayerRange() override{};
+    ~ClusterFilterByAlgoAndSizeAndLayerRange() override{};
 
     void filter(const std::vector<reco::CaloCluster>& layerClusters,
                 const HgcalClusterFilterMask& availableLayerClusters,
@@ -28,9 +30,12 @@ namespace ticl {
       auto filteredLayerClusters = std::make_unique<HgcalClusterFilterMask>();
       for (auto const& cl : availableLayerClusters) {
         auto const& layerCluster = layerClusters[cl.first];
-        auto layerId = rhtools.getLayerWithOffset(layerCluster.hitsAndFractions()[0].first);
+        auto const& haf = layerCluster.hitsAndFractions();
+        auto layerId = rhtools.getLayerWithOffset(haf[0].first);
+
         if (layerCluster.algo() == algo_number_ and layerId <= max_layerId_ and
-            layerId >= min_layerId_ ) {
+            layerId >= min_layerId_ and haf.size()<= max_cluster_size_ and
+            (haf.size() >= min_cluster_size_ or !(rhtools.isSilicon(haf[0].first)) ) ) {
           filteredLayerClusters->emplace_back(cl);
         } else {
           layerClustersMask[cl.first] = 0.;
@@ -40,8 +45,11 @@ namespace ticl {
 
   private:
     int algo_number_;
+    unsigned int min_cluster_size_;
+    unsigned int max_cluster_size_;
     unsigned int min_layerId_;
     unsigned int max_layerId_;
+
   };
 }  // namespace ticl
 
