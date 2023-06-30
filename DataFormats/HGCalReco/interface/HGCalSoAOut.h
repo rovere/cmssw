34393c66ec8fe47acdf683ba9471f044095bae9d
Marco Rovere <marco.rovere@cern.ch>
2023-06-30 09:01:02 +0200

    Put ClustersSoA in the Event. Clean dependencies.

--- DataFormats/HGCalReco/interface/HGCalSoAOut.h
+++ DataFormats/HGCalReco/interface/HGCalSoAOut.h
@@ -1,9 +1,6 @@
 #ifndef DataFormats_HGCalReco_interface_HGCalSoAOut_h
 #define DataFormats_HGCalReco_interface_HGCalSoAOut_h
 
-#include <Eigen/Core>
-#include <Eigen/Dense>
-
 #include "DataFormats/SoATemplate/interface/SoACommon.h"
 #include "DataFormats/SoATemplate/interface/SoALayout.h"
 #include "DataFormats/SoATemplate/interface/SoAView.h"
