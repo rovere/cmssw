5d6599bf079db3f988b2e7afe345b25a9faf5c73
Marco Rovere <marco.rovere@cern.ch>
2023-08-17 21:28:22 +0200

    Fix compilation failure due to sign extension.

--- DataFormats/HGCalReco/interface/HGCalSoAOut.h
+++ DataFormats/HGCalReco/interface/HGCalSoAOut.h
@@ -10,7 +10,7 @@
                       // columns: one value per element
                       SOA_COLUMN(float, delta),
                       SOA_COLUMN(float, rho),
-                      SOA_COLUMN(int, nearestHigher),
+                      SOA_COLUMN(unsigned int, nearestHigher),
                       SOA_COLUMN(int, clusterIndex),
                       SOA_COLUMN(uint8_t, isSeed),
                       SOA_SCALAR(unsigned int, numberOfClustersScalar)
