e10731b196263fc44e4a126c2fdfa3298ebebb15
Marco Rovere <marco.rovere@cern.ch>
2023-09-01 16:16:04 +0200

    Improve formatting of clusters dumping.

--- RecoLocalCalo/HGCalRecProducers/interface/DumpClustersDetails.h
+++ RecoLocalCalo/HGCalRecProducers/interface/DumpClustersDetails.h
@@ -7,18 +7,11 @@
 
 namespace hgcalUtils {
 
-  template<int N>
-    float roundToN(float value){
-      static float scale = std::pow(10., N);
-      return std::round(value * scale) / scale;
-    };
-
   static bool sortByDetId(const std::pair<int, float>& pair1, const std::pair<int, float>& pair2) {
-        return pair1.first < pair2.first;
+    return pair1.first < pair2.first;
   }
 
 
-  template<int N>
   class DumpClustersDetails {
     public:
       DumpClustersDetails() {};
@@ -27,19 +20,19 @@ namespace hgcalUtils {
         void dumpInfos(const T& clusters, bool dumpCellsDetId) const {
           int count=0;
           for (auto &i: clusters) {
-            std::cout << fmt::format("Seed: {}, Idx: {}, x: {}, y: {}, z: {}, eta: {}, phi: {}",
+            std::cout << fmt::format("Seed: {}, Idx: {}, x: {:a}, y: {:a}, z: {:a}, eta: {:a}, phi: {:a}",
                 i.seed().rawId(),
                 count++,
-                roundToN<N>(i.x()),
-                roundToN<N>(i.y()),
-                roundToN<N>(i.z()),
-                roundToN<N>(i.eta()),
-                roundToN<N>(i.phi()));
+                i.x(),
+                i.y(),
+                i.z(),
+                i.eta(),
+                i.phi());
             if (dumpCellsDetId) {
               auto sorted = i.hitsAndFractions(); // copy...
               std::stable_sort(std::begin(sorted), std::end(sorted), sortByDetId);
               for (auto const & c : sorted) {
-                std::cout << fmt::format(" ({}, {})", c.first, c.second) ;
+                std::cout << fmt::format(" ({}, {:a})", c.first, c.second) ;
               } // loop on hits and fractions
             }
             std::cout << std::endl;
@@ -47,7 +40,6 @@ namespace hgcalUtils {
         }
   };
 
-  template<int N>
     class DumpClustersSoADetails {
       public:
         DumpClustersSoADetails() {};
@@ -56,29 +48,18 @@ namespace hgcalUtils {
           void dumpInfos(const T& clustersSoA) const {
             for (int i = 0; i < clustersSoA->metadata().size(); ++i) {
               auto clusterSoAV = clustersSoA.view()[i];
-              std::cout << fmt::format("Idx: {}, delta: {:.{}f}, rho: {:.{}f}, nearest: {}, clsIdx: {}, isSeed: {}",
+              std::cout << fmt::format("Idx: {}, delta: {:.h}, rho: {:.h}, nearest: {}, clsIdx: {}, isSeed: {}",
                   i,
-                  clusterSoAV.delta(), std::numeric_limits<float>::max_digits10,
-                  clusterSoAV.rho(), std::numeric_limits<float>::max_digits10,
+                  clusterSoAV.delta(),
+                  clusterSoAV.rho(),
                   clusterSoAV.nearestHigher(),
                   clusterSoAV.clusterIndex(),
                   clusterSoAV.isSeed()
                   ) << std::endl;
-              /*
-              std::cout << fmt::format("Idx: {}, delta: {}, rho: {}, nearest: {}, clsIdx: {}, isSeed: {}",
-                  i,
-                  roundToN<N>(clusterSoAV.delta()),
-                  roundToN<N>(clusterSoAV.rho()),
-                  roundToN<N>(clusterSoAV.nearestHigher()),
-                  clusterSoAV.clusterIndex(),
-                  clusterSoAV.isSeed()
-                  ) << std::endl;
-              */
             }
           }
     };
 
-  template<int N>
     class DumpCellsSoADetails {
       public:
         DumpCellsSoADetails() {};
@@ -87,20 +68,19 @@ namespace hgcalUtils {
           void dumpInfos(const T& cells) const {
             for (int i = 0; i < cells->metadata().size(); ++i) {
               auto cellSoAV = cells.view()[i];
-              std::cout << fmt::format("Idx Cell: {}, x: {}, y: {}, layer: {}, weight: {}, sigmaNoise: {}, detid: {}",
+              std::cout << fmt::format("Idx Cell: {}, x: {:a}, y: {:a}, layer: {}, weight: {:a}, sigmaNoise: {:a}, detid: {}",
                   i,
-                  roundToN<N>(cellSoAV.dim1()),
-                  roundToN<N>(cellSoAV.dim2()),
+                  cellSoAV.dim1(),
+                  cellSoAV.dim2(),
                   cellSoAV.layer(),
-                  roundToN<N>(cellSoAV.weight()),
-                  roundToN<N>(cellSoAV.sigmaNoise()),
+                  cellSoAV.weight(),
+                  cellSoAV.sigmaNoise(),
                   cellSoAV.detid()
                   ) << std::endl;
             }
           }
     };
 
-  template<int N>
     class DumpLegacySoADetails {
       public:
         DumpLegacySoADetails() {};
@@ -109,15 +89,15 @@ namespace hgcalUtils {
           void dumpInfos(T & cells) const {
             for (unsigned int l = 0; l < cells.size(); l++) {
               for (unsigned int i = 0; i < cells.at(l).dim1.size(); ++i) {
-                std::cout << fmt::format("Idx Cell: {}, x: {}, y: {}, layer: {}, weight: {}, sigmaNoise: {}, delta: {:.{}f}, rho: {:.{}f}, nearestHigher: {}, clsIdx: {}, isSeed: {}, detid: {}",
+                std::cout << fmt::format("Idx Cell: {}, x: {:a}, y: {:a}, layer: {}, weight: {:a}, sigmaNoise: {:a}, delta: {:a}, rho: {:a}, nearestHigher: {}, clsIdx: {}, isSeed: {}, detid: {}",
                     i,
-                    roundToN<N>(cells.at(l).dim1.at(i)),
-                    roundToN<N>(cells.at(l).dim2.at(i)),
+                    cells.at(l).dim1.at(i),
+                    cells.at(l).dim2.at(i),
                     l,
-                    roundToN<N>(cells.at(l).weight.at(i)),
-                    roundToN<N>(cells.at(l).sigmaNoise.at(i)),
-                    cells.at(l).delta.at(i), std::numeric_limits<float>::max_digits10,
-                    cells.at(l).rho.at(i), std::numeric_limits<float>::max_digits10,
+                    cells.at(l).weight.at(i),
+                    cells.at(l).sigmaNoise.at(i),
+                    cells.at(l).delta.at(i),
+                    cells.at(l).rho.at(i),
                     cells.at(l).nearestHigher.at(i),
                     cells.at(l).clusterIndex.at(i),
                     cells.at(l).isSeed.at(i),
@@ -128,10 +108,10 @@ namespace hgcalUtils {
           }
     };
 
-  using DumpClusters = DumpClustersDetails<6>;
-  using DumpClustersSoA = DumpClustersSoADetails<6>;
-  using DumpCellsSoA = DumpCellsSoADetails<6>;
-  using DumpLegacySoA = DumpLegacySoADetails<6>;
+  using DumpClusters = DumpClustersDetails;
+  using DumpClustersSoA = DumpClustersSoADetails;
+  using DumpCellsSoA = DumpCellsSoADetails;
+  using DumpLegacySoA = DumpLegacySoADetails;
 }  // namespace hgcalUtils
 
 
