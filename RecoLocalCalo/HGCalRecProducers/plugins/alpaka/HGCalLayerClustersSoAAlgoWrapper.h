fea697ebfd0dd804be940276309f480f5ba5f589
Marco Rovere <marco.rovere@cern.ch>
2023-11-29 10:54:55 +0100

    Add SoA clusters Alpaka wrapper classes.

---
+++ RecoLocalCalo/HGCalRecProducers/plugins/alpaka/HGCalLayerClustersSoAAlgoWrapper.h
@@ -0,0 +1,24 @@
+#ifndef RecoLocalCalo_HGCalRecProducers_plugins_alpaka_HGCalLayerClustersSoAAlgoWrapper_h
+#define RecoLocalCalo_HGCalRecProducers_plugins_alpaka_HGCalLayerClustersSoAAlgoWrapper_h
+
+#include "DataFormats/HGCalReco/interface/HGCalSoACellsHostCollection.h"
+#include "DataFormats/HGCalReco/interface/alpaka/HGCalSoACellsDeviceCollection.h"
+#include "DataFormats/HGCalReco/interface/alpaka/HGCalSoAOutDeviceCollection.h"
+#include "DataFormats/HGCalReco/interface/alpaka/HGCalSoAClustersDeviceCollection.h"
+#include "HeterogeneousCore/AlpakaInterface/interface/config.h"
+
+namespace ALPAKA_ACCELERATOR_NAMESPACE {
+
+  class HGCalLayerClustersSoAAlgoWrapper {
+    public:
+      void run(Queue& queue,
+          const unsigned int numer_of_clusters,
+          const HGCalSoACellsDeviceCollection::ConstView input_rechits_soa,
+          const HGCalSoAOutDeviceCollection::ConstView input_clusters_soa,
+          HGCalSoAClustersDeviceCollection::View outputs
+          ) const;
+  };
+}  // namespace ALPAKA_ACCELERATOR_NAMESPACE
+
+#endif  // RecoLocalCalo_HGCalRecProducers_plugins_alpaka_HGCalLayerClustersSoAAlgoWrapper_h
+
---
+++ RecoLocalCalo/HGCalRecProducers/plugins/alpaka/HGCalSoALayerClustersProducer.cc
@@ -0,0 +1,74 @@
+#include "DataFormats/PortableTestObjects/interface/alpaka/TestDeviceCollection.h"
+#include "FWCore/Framework/interface/ConsumesCollector.h"
+#include "FWCore/ParameterSet/interface/ConfigurationDescriptions.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
+#include "FWCore/Utilities/interface/InputTag.h"
+#include "HeterogeneousCore/AlpakaCore/interface/alpaka/stream/EDProducer.h"
+#include "HeterogeneousCore/AlpakaCore/interface/alpaka/EDPutToken.h"
+#include "HeterogeneousCore/AlpakaCore/interface/alpaka/ESGetToken.h"
+#include "HeterogeneousCore/AlpakaInterface/interface/config.h"
+#include "HeterogeneousCore/AlpakaTest/interface/AlpakaESTestRecords.h"
+#include "HeterogeneousCore/AlpakaTest/interface/alpaka/AlpakaESTestData.h"
+#include "RecoLocalCalo/HGCalRecProducers/interface/HGCalTilesConstants.h"
+#include "Geometry/HGCalGeometry/interface/HGCalGeometry.h"
+#include "DataFormats/HGCRecHit/interface/HGCRecHitCollections.h"
+
+#include "DataFormats/HGCalReco/interface/HGCalSoACellsHostCollection.h"
+#include "DataFormats/HGCalReco/interface/alpaka/HGCalSoAClustersDeviceCollection.h"
+#include "DataFormats/HGCalReco/interface/alpaka/HGCalSoAOutDeviceCollection.h"
+#include "HGCalLayerClustersSoAAlgoWrapper.h"
+
+
+namespace ALPAKA_ACCELERATOR_NAMESPACE {
+
+  class HGCalSoALayerClustersProducer : public stream::EDProducer<> {
+    public:
+      HGCalSoALayerClustersProducer(edm::ParameterSet const& config)
+        : getTokenDeviceRecHits_{consumes(config.getParameter<edm::InputTag>("hgcalRecHitsSoA"))},
+          getTokenDeviceClusters_{consumes(config.getParameter<edm::InputTag>("hgcalLayerClustersSoA"))},
+          deviceTokenSoAClusters_{produces()}{}
+
+      ~HGCalSoALayerClustersProducer() override = default;
+
+      void produce(device::Event& iEvent, device::EventSetup const& iSetup) override {
+
+        // Get RecHitsSoA on the device
+        auto const & deviceInputRecHits = iEvent.get(getTokenDeviceRecHits_);
+        auto const inputRechits_v = deviceInputRecHits.view();
+
+        // Get LayerClusters almost-SoA on device: this has still the same
+        // cardinality as the RecHitsSoA, but has all the required information
+        // to assemble the clusters, i.e., it has the cluster index assigned to
+        // each rechit.
+        auto const & deviceInputClusters = iEvent.get(getTokenDeviceClusters_);
+        auto const inputClusters_v = deviceInputClusters.view();
+        //
+        // Allocate output SoA for the clusters, one entry for each cluster
+        ALPAKA_ACCELERATOR_NAMESPACE::PortableCollection<HGCalClustersSoA> output(deviceInputClusters.view().numberOfClustersScalar(), iEvent.queue());
+        auto output_v = output.view();
+
+        algo_.run(iEvent.queue(),
+            deviceInputClusters.view().numberOfClustersScalar(),
+            inputRechits_v, inputClusters_v, output_v);
+        iEvent.emplace(deviceTokenSoAClusters_, std::move(output));
+      }
+
+
+      static void fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+        edm::ParameterSetDescription desc;
+        desc.add<edm::InputTag>("hgcalLayerClustersSoA", edm::InputTag("TO BE DEFINED"));
+        descriptions.addWithDefaultLabel(desc);
+      }
+
+    private:
+      device::EDGetToken<ALPAKA_ACCELERATOR_NAMESPACE::PortableCollection<HGCalCellsSoA>> const getTokenDeviceRecHits_;
+      device::EDGetToken<ALPAKA_ACCELERATOR_NAMESPACE::PortableCollection<HGCalCellsOutSoA>> const getTokenDeviceClusters_;
+      device::EDPutToken<ALPAKA_ACCELERATOR_NAMESPACE::PortableCollection<HGCalClustersSoA>> const deviceTokenSoAClusters_;
+      HGCalLayerClustersSoAAlgoWrapper algo_;
+  };
+
+}  // namespace ALPAKA_ACCELERATOR_NAMESPACE
+
+#include "HeterogeneousCore/AlpakaCore/interface/alpaka/MakerMacros.h"
+DEFINE_FWK_ALPAKA_MODULE(HGCalSoALayerClustersProducer);
