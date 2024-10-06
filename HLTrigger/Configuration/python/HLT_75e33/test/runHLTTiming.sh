c1992c7fcd4f06b0495e52591bd94ca64bfb3b61
Marco Rovere <marco.rovere@cern.ch>
2024-10-06 06:48:05 +0200

    Add HLT on Bergamo for L1A 140PU from Spring24.

--- HLTrigger/Configuration/python/HLT_75e33/test/runHLTTiming.sh
+++ HLTrigger/Configuration/python/HLT_75e33/test/runHLTTiming.sh
@@ -24,10 +24,8 @@ set -o pipefail
 ###MR     fi
 ###MR   fi
 ###MR done
- 
+###MR 
 ###MR LOCALPATH=${FOLDER_FILES}$(dirname ${FILES[0]})
-###MR LOCALPATH="/data/user/rovere/store/mc/Phase2Spring24DIGIRECOMiniAOD/TT_TuneCP5_14TeV-powheg-pythia8/GEN-SIM-DIGI-RAW-MINIAOD/PU200_AllTP_140X_mcRun4_realistic_v4-v1/2560000"
-###MR ###MR LOCALPATH="/data/user/rovere/store/relval/CMSSW_14_1_0_pre6/RelValMinBias_14TeV/GEN-SIM-DIGI-RAW/PU_141X_mcRun4_realistic_v1_STD_2026D110_PU-v3"
 ###MR echo "Local repository: |${LOCALPATH}|"
 ###MR LOCALFILES=$(ls -1 ${LOCALPATH})
 ###MR ALL_FILES=""
@@ -37,21 +35,21 @@ set -o pipefail
 ###MR # Remove the last character
 ###MR ALL_FILES="${ALL_FILES%?}"
 ###MR echo "Discovered files: $ALL_FILES"
- 
+###MR 
 ###MR cmsDriver.py Phase2 -s L1P2GT,HLT:75e33_timing --processName=HLTX \
 ###MR   --conditions auto:phase2_realistic_T33 --geometry Extended2026D110 \
 ###MR   --era Phase2C17I13M9 \
 ###MR   --customise SLHCUpgradeSimulations/Configuration/aging.customise_aging_1000 \
 ###MR   --eventcontent FEVTDEBUGHLT \
 ###MR   --filein=${ALL_FILES} \
-###MR   --mc --nThreads 8 --inputCommands='keep *, drop *_hlt*_*_HLT, drop triggerTriggerFilterObjectWithRefs_l1t*_*_HLT' \
-###MR   -n 1000 --no_exec --output={} --procModifier alpaka
+###MR   --mc --nThreads 4 --inputCommands='keep *, drop *_hlt*_*_HLT, drop triggerTriggerFilterObjectWithRefs_l1t*_*_HLT' \
+###MR   -n 1000 --no_exec --output={}
 
-if [ -e 'Phase2_L1P2GT_HLT_L1ASkimReducedMenu_200PU.py' ]; then
+if [ -e 'Phase2_L1P2GT_HLT_L1ASkimReducedMenu.py' ]; then
   if [ ! -d 'patatrack-scripts' ]; then
     git clone https://github.com/cms-patatrack/patatrack-scripts --depth 1
   fi
-  patatrack-scripts/benchmark -j 32 -t 8 -s 8 -e 1000 --run-io-benchmark --event-skip 100 --event-resolution 25 -k Phase2Timing_resources.json -- Phase2_L1P2GT_HLT_L1ASkimReducedMenu_200PU.py
+  patatrack-scripts/benchmark -j 64 -t 8 -s 8 -e 1000 --run-io-benchmark -k Phase2Timing_resources.json --event-skip 100 --event-resolution 25 -- Phase2_L1P2GT_HLT_L1ASkimReducedMenu.py
   mergeResourcesJson.py logs/step*/pid*/Phase2Timing_resources.json > Phase2Timing_resources.json
   if [ -e "$(dirname $0)/augmentResources.py" ]; then
     python3 $(dirname $0)/augmentResources.py
