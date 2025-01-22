8e84f765a2397388859652e026ff5a9d0ea963cf
Luca Ferragina <ferragina.luca@gmail.com>
2025-01-22 10:54:43 +0100

    Fix incorrect types in hltPhase2PixelTracksSoA_cfi

--- HLTrigger/Configuration/python/HLT_75e33/modules/hltPhase2PixelTracksSoA_cfi.py
+++ HLTrigger/Configuration/python/HLT_75e33/modules/hltPhase2PixelTracksSoA_cfi.py
@@ -13,22 +13,22 @@ hltPhase2PixelTracksSoA = cms.EDProducer('CAHitNtupletAlpakaPhase2@alpaka',
     lateFishbone = cms.bool(False),
     fillStatistics = cms.bool(False),
     minHitsPerNtuplet = cms.uint32(4),
-    minz = cms.vint32(
+    cellMinz = cms.vint32(
         -16, 4, -22, -17, 6,-22, -18, 11, -22, 23, 30, 39, 50,
         65, 82, 109, -28, -35, -44, -55, -70, -87, -113, -16, 
         7, -22, 11,-22, -17, 9,-22, 13, -22, 137, 173, 199, 229,
         -142, -177, -203, -233, 23, 30, 39, 50, 65, 82, 109, -28,
         -35, -44, -55, -70, -87, -113
     ),
-    maxz = cms.vint32(
+    cellMaxz = cms.vint32(
         17, 22, -4, 17, 22, -6, 18, 22, -11, 28, 35, 44, 55, 70,
         87, 113, -23, -30, -39, -50, -65, -82, -109, 17, 22, -7,
         22, -10, 17, 22, -9, 22, -13, 142, 177, 203, 233, -137,
         -173, -199, -229, 28, 35, 44, 55, 70, 87, 113, -23, -30,
         -39, -50, -65, -82, -109
     ),
-    cellMinYSizeB1 = cms.double(25),
-    cellMinYSizeB2 = cms.double(15),
+    cellMinYSizeB1 = cms.int32(25),
+    cellMinYSizeB2 = cms.int32(15),
     cellZ0Cut = cms.double(7.5),
     phiCuts = cms.vint32(
         522, 522, 522, 626, 730, 730, 626, 730, 730, 522, 522,
@@ -37,9 +37,9 @@ hltPhase2PixelTracksSoA = cms.EDProducer('CAHitNtupletAlpakaPhase2@alpaka',
         730, 730, 730, 730, 730, 730, 730, 730, 730, 730, 730,
         730, 730, 730, 522, 522, 522, 522, 522, 522, 522, 522
     ),
-    cellMaxDYSize12 = cms.double(12),
-    cellMaxDYSize = cms.double(10),
-    cellMaxDYPred = cms.double(20),
+    cellMaxDYSize12 = cms.int32(12),
+    cellMaxDYSize = cms.int32(10),
+    cellMaxDYPred = cms.int32(20),
     cellPtCut = cms.double(0.85),
     maxNumberOfDoublets = cms.uint32(5*512*1024),
     minHitsForSharingCut = cms.uint32(10),
