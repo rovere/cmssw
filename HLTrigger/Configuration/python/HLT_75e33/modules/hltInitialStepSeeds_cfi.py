59753896130c0f983e5cae9aa3801787ca64d18c
Marco Musich <marco.musich@cern.ch>
2025-07-31 14:24:00 +0200

    add removeOTHits to SeedGeneratorFromProtoTracksEDProducer

--- HLTrigger/Configuration/python/HLT_75e33/modules/hltInitialStepSeeds_cfi.py
+++ HLTrigger/Configuration/python/HLT_75e33/modules/hltInitialStepSeeds_cfi.py
@@ -12,11 +12,15 @@ hltInitialStepSeeds = cms.EDProducer("SeedGeneratorFromProtoTracksEDProducer",
     useEventsWithNoVertex = cms.bool(True),
     usePV = cms.bool(False),
     useProtoTrackKinematics = cms.bool(False),
-    includeFourthHit = cms.bool(False)
+    includeFourthHit = cms.bool(False),
+    removeOTRechits = cms.bool(False)
 )
 
 from Configuration.ProcessModifiers.trackingLST_cff import trackingLST
 trackingLST.toModify(hltInitialStepSeeds, includeFourthHit = True)
+
+from Configuration.ProcessModifiers.ngtScouting_cff import ngtScouting
+ngtScouting.toModify(hltInitialStepSeeds, removeOTRechits = True)
 
 from Configuration.ProcessModifiers.phase2_hlt_vertexTrimming_cff import phase2_hlt_vertexTrimming
 phase2_hlt_vertexTrimming.toModify(hltInitialStepSeeds, InputVertexCollection = "hltPhase2TrimmedPixelVertices")
